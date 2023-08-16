4c433efc41a1d1d297e55823d9073e193a6a8c55
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-08-16 15:03:41 +0200

    Add fran

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # placeneko
 <p  align="center">
-  <img src="/nekos/beast-tamer/kanade/1668283120111389.jpg" />
+  <img src="/nekos/tenken/fran/1667132468121990.jpg" />
 </p>
--- app/api/random/route.ts
+++ app/api/random/route.ts
@@ -4,13 +4,21 @@ import path from "path";
 
 export const dynamic = "force-dynamic";
 
-const nekoDir = path.join(process.cwd(), "nekos/beast-tamer/kanade");
-const nekos = readdirSync(nekoDir);
+const kanade = readdirSync(
+  path.join(process.cwd(), "nekos/beast-tamer/kanade"),
+  { withFileTypes: true },
+);
+
+const fran = readdirSync(path.join(process.cwd(), "nekos/tenken/fran"), {
+  withFileTypes: true,
+});
+
+const nekos = [...kanade, ...fran];
 
 export const GET = async () => {
   const randomIndex = Math.floor(Math.random() * nekos.length);
 
-  const nekoPath = path.join(nekoDir, nekos[randomIndex]);
+  const nekoPath = path.join(nekos[randomIndex].path, nekos[randomIndex].name);
   const nekoBuffer = readFileSync(nekoPath);
 
   return new Response(nekoBuffer, {
